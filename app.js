4ba51d5b2a82a71c37f04799b4727562e6926d0f
Eva <126707187+evakarka@users.noreply.github.com>
2024-05-18 23:31:33 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const mysql = require('mysql2');
 
-// Δημιουργία σύνδεσης με τη βάση δεδομένων MySQL
 const connection = mysql.createConnection({
-  host: 'localhost', // Ανάλογα με τη διεύθυνση του MySQL server
-  user: 'alumnimemo', // Το όνομα χρήστη της βάσης δεδομένων
-  password: '', // Ο κωδικός πρόσβασης της βάσης δεδομένων
-  database: 'your_database' // Το όνομα της βάσης δεδομένων που θέλετε να συνδεθείτε
+  host: 'localhost', 
+  user: 'alumnimemo', 
+  password: '', 
+  database: 'your_database' 
 });
 
-// Δοκιμαστική ερώτηση προς τη βάση δεδομένων
+
 connection.query(
-  'SELECT * FROM your_table', // Αντικαταστήστε το your_table με το όνομα του πίνακα που θέλετε να επιλέξετε
+  'SELECT * FROM your_table', 
   function(err, results, fields) {
     if (err) {
       console.error('Error connecting to MySQL: ' + err.stack);
@@ -21,5 +20,4 @@ connection.query(
   }
 );
 
-// Κλείσιμο της σύνδεσης με τη βάση δεδομένων MySQL μετά την ολοκλήρωση των ερωτημάτων
 connection.end();
